a2de16dc4d1cd65b458a70f7c4c49d812edceca6
sourav-bhattacharya-github <02sourav.bhattacharya@gmail.com>
2025-06-09 02:45:09 +0530

    add new folder in String qurstion:: First non-repeating character

--- DSA in C++ by Love Babbar/day-25/Linklist/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    // inserNode(tail, 3, 5);
-    // inserNode(tail, 5, 7);
-    // inserNode(tail, 7, 9);
-    // inserNode(tail, 5, 6);
-    // inserNode(tail, 3, 2);
---
+++ DSA/GFG/String/First_non-repeating_character.cpp
@@ -0,0 +1,37 @@
+// Questions Link: https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/article/MTc4MQ%3D%3D
+
+class Solution
+{
+    const int MAX_CHAR = 26;
+
+public:
+    int firstUniqChar(string s)
+    {
+        vector<int> vis(MAX_CHAR, -1);
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            int index = s[i] - 'a';
+
+            if (vis[index] == -1)
+            {
+                vis[index] = i;
+            }
+            else
+            {
+                vis[index] = -2;
+            }
+        }
+
+        int idx = -1;
+        for (int i = 0; i < MAX_CHAR; i++)
+        {
+            if (vis[i] >= 0 && (idx == -1 || vis[i] < vis[idx]))
+            {
+                idx = i;
+            }
+        }
+
+        return (idx == -1) ? -1 : vis[idx];
+    }
+};
