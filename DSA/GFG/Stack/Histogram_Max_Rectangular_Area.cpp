2f19bd7fcaafdccdba976eb2277ad855e68da41e
sourav-bhattacharya-github <02sourav.bhattacharya@gmail.com>
2025-09-04 22:19:24 +0530

    modify

--- DSA/GFG/Stack/Histogram_Max_Rectangular_Area.cpp
+++ DSA/GFG/Stack/Histogram_Max_Rectangular_Area.cpp
@@ -7,34 +7,33 @@ using namespace std;
 
 int getMaxArea(vector<int> &arr)
 {
+    // Your code here
     int n = arr.size();
     stack<int> s;
-    int res = 0;
-    int tp, curr;
+
+    int res = 0, tp, curr;
+
     for (int i = 0; i < n; i++)
     {
 
         while (!s.empty() && arr[s.top()] >= arr[i])
         {
-
-            // The popped item is to be considered as the
-            // smallest element of the Histogram
+            // The popped item is to be considered as
+            // the smallest element of te Histogram
             tp = s.top();
             s.pop();
 
-            // For the popped item previous smaller element is
-            // just below it in the stack (or current stack top)
-            // and next smaller element is i
+            // For the popped item previous smaller is just below in the stack
+            // (or current stack top)
+            // and next smallest element is i
             int width = s.empty() ? i : i - s.top() - 1;
 
             res = max(res, arr[tp] * width);
         }
         s.push(i);
     }
-
-    // For the remaining items in the stack, next smaller does
-    // not exist. Previous smaller is the item just below in
-    // stack.
+    // For the remaning items in the stack, next smaller does not exist.
+    //  Previous smaller is item just below in stack
     while (!s.empty())
     {
         tp = s.top();
@@ -42,7 +41,6 @@ int getMaxArea(vector<int> &arr)
         curr = arr[tp] * (s.empty() ? n : n - s.top() - 1);
         res = max(res, curr);
     }
-
     return res;
 }
 
